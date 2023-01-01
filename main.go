e84584483e17b3ce15acc5a3e9f9123541831a4d
Kevin <kelvinian87@gmail.com>
2023-01-01 18:52:31 +0300

    now learning arrays and slices in Go

--- main.go
+++ main.go
@@ -5,8 +5,8 @@ import "fmt"
 func main() {
 
 	var conferenceName = "Go Conference"
-	const conferenceTickets int = 50
-	var remainingTickets = 50
+	const conferenceTickets uint = 50
+	var remainingTickets uint = 50
 
 	/*Assigning datatypes to variables ensures that we protect our variables from getting a value they're not supposed to get. */
 	fmt.Printf("Welcome to the %v. We have a total of %v tickets, and %v ticktes remain so far.\n", conferenceName, conferenceTickets, remainingTickets)
@@ -25,7 +25,7 @@ func main() {
 	var firstName string
 	var lastName string
 	var email string
-	var userTickets int
+	var userTickets uint
 
 	fmt.Println("Enter your first name to proceed")
 	fmt.Scan(&firstName)
@@ -43,4 +43,6 @@ func main() {
 
 	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v.\n", firstName, lastName, userTickets, email)
 	fmt.Printf("We have %v remaining tickets for the %v.\n", remainingTickets, conferenceName)
+
+	//arrays and slices
 }
